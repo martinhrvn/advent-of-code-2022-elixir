c77c77e005413eeb70d98c1de1de7a17d7ef53a6
Martin Harvan <martin.harvan1@ingka.com>
2022-12-02 22:56:18 +0100

    Adding day 1 and day 2

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,22 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  def part1(lines) do
+    lines |> top_calories |> hd
   end
 
-  def part2(_args) do
+  def part2(lines) do
+    lines |> top_calories |> Enum.take(3) |> Enum.sum()
+  end
+
+  defp top_calories(lines) do
+    lines
+    |> String.split("\n")
+    |> Enum.chunk_by(fn l -> l == "" end)
+    |> Enum.map(fn arr ->
+      arr
+      |> Enum.filter(fn n -> n != "" end)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sum()
+    end)
+    |> Enum.sort(:desc)
   end
 end
--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,72 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  @win "Z"
+  @loss "X"
+  @draw "Y"
+
+  @op_rock "A"
+  @op_paper "B"
+  @op_scissors "C"
+
+  @my_rock "X"
+  @my_paper "Y"
+  @my_scissors "Z"
+
+  @game_scores %{@win => 6, @loss => 0, @draw => 3}
+  @shape_props %{
+    @op_rock => {@my_scissors, @my_rock, @my_paper},
+    @op_paper => {@my_rock, @my_paper, @my_scissors},
+    @op_scissors => {@my_paper, @my_scissors, @my_rock}
+  }
+
+  def part1(lines) do
+    lines
+    |> parse_input()
+    |> Enum.map(fn
+      [a, b] -> game_result(a, b) + shape_score(b)
+    end)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(lines) do
+    lines
+    |> parse_input()
+    |> Enum.map(fn
+      [a, result] ->
+        @game_scores[result] + (shape_for_result(a, result) |> shape_score())
+    end)
+    |> Enum.sum()
+  end
+
+  defp shape_score(shape) do
+    case shape do
+      "X" -> 1
+      "Y" -> 2
+      "Z" -> 3
+    end
+  end
+
+  defp shape_for_result(a, result) do
+    {loss, draw, win} = @shape_props[a]
+
+    case result do
+      @win -> win
+      @loss -> loss
+      @draw -> draw
+    end
+  end
+
+  defp parse_input(lines) do
+    lines
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.split(&1, " "))
+  end
+
+  defp game_result(a, b) do
+    case @shape_props[a] do
+      {_, _, ^b} -> @game_scores[@win]
+      {^b, _, _} -> @game_scores[@loss]
+      _ -> @game_scores[@draw]
+    end
   end
 end
