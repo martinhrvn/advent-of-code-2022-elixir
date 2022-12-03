b2e57dc873a40cb9290c2b127c19d4a4303b5a05
Martin Harvan <martin.harvan@care.com>
2022-12-03 14:49:42 +0100

    Day 3

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,7 +1,38 @@
 defmodule AdventOfCode.Day03 do
-  def part1(_args) do
+  def part1(input) do
+    input
+    |> parse_input()
+    |> Enum.map(fn line ->
+      line
+      |> then(&Enum.split(&1, div(length(&1), 2)))
+      |> then(fn {a, b} -> intersect(a, b) end)
+      |> hd()
+      |> get_points()
+    end)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(input) do
+    input
+    |> parse_input()
+    |> Enum.chunk_every(3)
+    |> Enum.map(fn [a, b, c] -> intersect(a, b) |> intersect(c) |> hd |> get_points end)
+    |> Enum.sum()
+  end
+
+  defp parse_input(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.to_charlist/1)
+  end
+
+  defp get_points(char) when char in ?a..?z, do: char - ?a + 1
+  defp get_points(char) when char in ?A..?Z, do: char - ?A + 27
+
+  @spec intersect(list, list) :: list
+  defp intersect(a, b) do
+    diff = a -- b
+    a -- diff
   end
 end
