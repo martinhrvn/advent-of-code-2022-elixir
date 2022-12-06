ced18388005331c1dce4b9dfdab372783417e077
Martin Harvan <martin.harvan@care.com>
2022-12-06 09:56:22 +0100

    Day 6

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,25 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+  def part1(input) do
+    input
+    |> find_first_distsinct(4)
   end
 
-  def part2(_args) do
+  def part2(input) do
+    input
+    |> find_first_distsinct(14)
+  end
+
+  defp find_first_distsinct(input, n) do
+    input
+    |> String.to_charlist()
+    |> Enum.reduce_while({'', 1}, fn char, {acc, count} ->
+      chars = [char | Enum.take(acc, n - 1)]
+
+      if length(Enum.uniq(chars)) == n do
+        {:halt, count}
+      else
+        {:cont, {chars, count + 1}}
+      end
+    end)
   end
 end
