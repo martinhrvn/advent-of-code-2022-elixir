edc3b722f1daa75b7f8533c2bad9146c55885742
Martin Harvan <martin.harvan@care.com>
2022-12-08 10:02:39 +0100

    Day 8 part 1

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -1,5 +1,48 @@
 defmodule AdventOfCode.Day08 do
-  def part1(_args) do
+  def part1(input) do
+    grid =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.split(&1, "", trim: true))
+
+    width = Enum.at(grid, 0) |> length()
+    height = grid |> length()
+
+    grid_map =
+      grid
+      |> Enum.with_index()
+      |> Enum.map(fn {row, y} ->
+        row
+        |> Enum.with_index()
+        |> Enum.map(fn {pixel, x} ->
+          {{x, y}, String.to_integer(pixel)}
+        end)
+        |> Enum.into(%{})
+      end)
+      |> Enum.reduce(%{}, fn row, acc -> Map.merge(acc, row) end)
+
+    grid_map |> Enum.count(fn {k, _v} -> is_visible?(grid_map, k, {width, height}) end)
+  end
+
+  defp are_all_lower?(grid, coordinates, val) do
+    grid
+    |> Map.take(coordinates)
+    |> Enum.all?(fn {_, v} -> v < val end)
+  end
+
+  defp is_visible?(_, {0, _}, _), do: true
+  defp is_visible?(_, {_, 0}, _), do: true
+  defp is_visible?(_, {_, y}, {_, height}) when y == height - 1, do: true
+  defp is_visible?(_, {x, _}, {width, _}) when x == width - 1, do: true
+
+  defp is_visible?(grid, {x, y}, {width, height}) do
+    val = grid[{x, y}]
+    from_north = are_all_lower?(grid, Enum.map(0..(y - 1), &{x, &1}), val)
+    from_south = are_all_lower?(grid, Enum.map((y + 1)..(height - 1), &{x, &1}), val)
+    from_west = are_all_lower?(grid, Enum.map(0..(x - 1), &{&1, y}), val)
+    from_east = are_all_lower?(grid, Enum.map((x + 1)..(width - 1), &{&1, y}), val)
+
+    from_north or from_south or from_west or from_east
   end
 
   def part2(_args) do
--- test/advent_of_code/day_08_test.exs
+++ test/advent_of_code/day_08_test.exs
@@ -3,12 +3,18 @@ defmodule AdventOfCode.Day08Test do
 
   import AdventOfCode.Day08
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = """
+    30373
+    25512
+    65332
+    33549
+    35390
+    """
+
     result = part1(input)
 
-    assert result
+    assert result == 21
   end
 
   @tag :skip
