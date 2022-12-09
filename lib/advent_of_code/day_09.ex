35d1f061dbecbbce520e2e4e9a279607e28f6957
Martin Harvan <martin.harvan@care.com>
2022-12-09 11:00:39 +0100

    Day 9 incomplete

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -1,7 +1,50 @@
 defmodule AdventOfCode.Day09 do
-  def part1(_args) do
+  def part1(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      [direction, count] = line |> String.split(" ", trim: true)
+      {direction, String.to_integer(count)}
+    end)
+    |> Enum.flat_map(fn {d, c} -> List.duplicate({d, c}, c) end)
+    |> Enum.reduce({%{}, {0, 0}, {0, 0}}, fn {d, _}, {visited, head, tail} ->
+      move(visited, head, tail, dir(d))
+    end)
+    |> then(fn {visited, _, _} -> map_size(visited) end)
   end
 
   def part2(_args) do
   end
+
+  defp dir("R"), do: {0, 1}
+  defp dir("L"), do: {0, -1}
+  defp dir("U"), do: {1, 0}
+  defp dir("D"), do: {-1, 0}
+
+  defp move(visited, {hx, hy}, {tx, ty}, {dx, dy}) do
+    head = {hx + dx, hy + dy}
+
+    new_tail = calculate_tail(head, {tx, ty})
+    IO.inspect(head, label: "head")
+    IO.inspect(new_tail, label: "tail")
+    visited = Map.put(visited, new_tail, true)
+    {visited, head, new_tail}
+  end
+
+  defp calculate_tail({hx, hy}, {tx, ty}) do
+    dx = hx - tx
+    dy = hy - ty
+    # IO.inspect({dx, dy, clamp(dx), clamp(dy)}, label: "diff")
+
+    cond do
+      abs(dx) < 2 and abs(dy) < 2 -> {tx, ty}
+      abs(dx) < 2 -> {hx, hy - clamp(dy)}
+      abs(dy) < 2 -> {hx - clamp(dx), hy}
+      true -> {hx - clamp(dx), hy - clamp(dy)}
+    end
+  end
+
+  defp clamp(0), do: 0
+  defp clamp(n) when n > 0, do: 1
+  defp clamp(_), do: -1
 end
